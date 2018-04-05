c450dde62d600c78e211c8abdc8b85b7ea1d2efd
Akhmad Akbar Alfarizi <32434489+akbaralfarizi@users.noreply.github.com>
2018-04-05 13:44:40 +0700

    Mengisi Nilai Atribut

--- src/praktek01/sepatuAksi.java
+++ src/praktek01/sepatuAksi.java
@@ -12,7 +12,24 @@ package praktek01;
 public class sepatuAksi {
     public static void main(String[] args) {
         sepatu jalan = new sepatu();
+            jalan.merk="Adidas";
+            jalan.warna="Merah";
+            jalan.bahan="Kulit";
+            jalan.size="41";
+            jalan.madein="EUR";
+            
         sepatu sekolah = new sepatu();
+            sekolah.merk="Nike";
+            sekolah.warna="Hitam";
+            sekolah.bahan="Kain";
+            sekolah.size="40";
+            sekolah.madein="Vietnam";
+            
         sepatu kerja = new sepatu();
+            jalan.merk="Converse";
+            jalan.warna="Biru";
+            jalan.bahan="Canvas";
+            jalan.size="41";
+            jalan.madein="Indonesia";
     }
 }
